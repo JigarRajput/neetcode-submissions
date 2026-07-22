cbeb2721ddbeedcf0d45e8edcbdeabfe8ed4b2f2
Jigar Subhash Rajput <80737219+JigarRajput@users.noreply.github.com>
2026-07-22 08:09:49 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.java
@@ -0,0 +1,32 @@
+// Jai Ganesh
+class Solution {
+
+    void solve(int target, List<Integer> currentElements, int[] candidates, List<List<Integer>> ans, int index) {
+        // while target is > 0 at every recursion step we have choice to include any element and move on
+
+        if(target == 0) {
+            ans.add(currentElements);
+            return;
+        }
+
+        for(int ind = index; ind < candidates.length; ind++) {
+            if(target > 0 && target >= candidates[ind]) {
+                int updatedTarget = target - candidates[ind];
+                List<Integer> elements = new ArrayList<>();
+                elements.addAll(currentElements);
+                elements.add(candidates[ind]);
+                solve(updatedTarget, elements, candidates, ans, ind);
+            }
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> currentElements = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        solve(target, currentElements, candidates, ans, 0);
+
+        return ans;
+    }
+}
